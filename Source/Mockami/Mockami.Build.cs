b0fd81cfc937ddd8620e04fa7b53c0c0b57481b9
Blue_Gene <khalifaabdallah101@gmail.com>
2024-04-28 06:20:53 +0300

    Initial commit

---
+++ Source/Mockami/Mockami.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Mockami : ModuleRules
+{
+	public Mockami(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/Mockami/MockamiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MockamiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMockamiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMockamiGameMode();
+};
+
+
+
